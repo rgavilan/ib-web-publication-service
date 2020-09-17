39159b0f95be3f1c20200cc5386f79ae453cf111
Rubén Gavilán <rgavilan@izertis.com>
2020-09-17 11:53:46 +0200

    Fix default output path

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  urlBase: 'http://localhost:8080',
+  urlWebsocket: '/gs-guide-websocket',
+  oauthClientId: 'acme',
+  oauthSecret: 'acmesecret'
+};
+
+// default settings for how to query the endpoint
+export const yasgui = {
+  endpoint: 'http://herc-iz-front-desa.atica.um.es/proxy/wdqs/bigdata/namespace/wdq/sparql'
+}
