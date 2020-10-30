6e25dd52c96fadad506ec9b8c942d387419c8cc3
Alejandro Martino Uria <amartino@izertis.com>
2020-10-30 11:50:31 +0100

    SEMANTMURC-357 clean up

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -8,11 +8,8 @@ export const environment = {
 
 // default settings for how to query the endpoint
 export const yasgui = {
-  // Wikibase:
-  // endpoint: 'http://herc-iz-front-desa.atica.um.es/proxy/wdqs/bigdata/namespace/wdq/sparql',
-  // method: 'GET'
 
   // Fuseki
-  endpoint: 'http://herc-iz-bd-desa.atica.um.es:3030/trellis/query',
+  endpoint: 'https://query.wikidata.org/sparql',
   method: 'POST'
 }
