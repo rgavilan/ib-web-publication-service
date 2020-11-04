4adaa29ce76563f2d42554020ff66695cbe158a7
Alejandro Martino Uria <amartino@izertis.com>
2020-11-04 12:12:18 +0100

    SEMANTMURC-357 sector graphic 2

--- src/app/cartegories/researchment-structures/researchment-structures.component.ts
+++ src/app/cartegories/researchment-structures/researchment-structures.component.ts
@@ -73,12 +73,11 @@ export class ResearchmentStructuresComponent extends PaginatedSearchComponent<
       data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
     }
 
-    const data = this.genData(50);
+    const data = this.genData(5);
 
     this.echartOptions = {
       title: {
-        text: '同名数量统计',
-        subtext: '纯属虚构',
+        text: 'Núm. Universidades [Sello de Calidad]',
         left: 'center',
       },
       tooltip: {
@@ -87,24 +86,21 @@ export class ResearchmentStructuresComponent extends PaginatedSearchComponent<
       },
       legend: {
         type: 'scroll',
-        orient: 'vertical',
+        orient: 'horizontal',
         right: 10,
-        top: 20,
-        bottom: 20,
+        top: 30,
+        bottom: 0,
         data: data.legendData,
-        // data: null,
 
         selected: data.selected,
-        // selected: null,
       },
       series: [
         {
-          name: '姓名',
+          name: 'Sello de Calidad',
           type: 'pie',
           radius: '55%',
           center: ['40%', '50%'],
           data: data.seriesData,
-          //data: null,
           emphasis: {
             itemStyle: {
               shadowBlur: 10,
@@ -123,146 +119,11 @@ export class ResearchmentStructuresComponent extends PaginatedSearchComponent<
    */
   genData(count) {
     var nameList = [
-      '赵',
-      '钱',
-      '孙',
-      '李',
-      '周',
-      '吴',
-      '郑',
-      '王',
-      '冯',
-      '陈',
-      '褚',
-      '卫',
-      '蒋',
-      '沈',
-      '韩',
-      '杨',
-      '朱',
-      '秦',
-      '尤',
-      '许',
-      '何',
-      '吕',
-      '施',
-      '张',
-      '孔',
-      '曹',
-      '严',
-      '华',
-      '金',
-      '魏',
-      '陶',
-      '姜',
-      '戚',
-      '谢',
-      '邹',
-      '喻',
-      '柏',
-      '水',
-      '窦',
-      '章',
-      '云',
-      '苏',
-      '潘',
-      '葛',
-      '奚',
-      '范',
-      '彭',
-      '郎',
-      '鲁',
-      '韦',
-      '昌',
-      '马',
-      '苗',
-      '凤',
-      '花',
-      '方',
-      '俞',
-      '任',
-      '袁',
-      '柳',
-      '酆',
-      '鲍',
-      '史',
-      '唐',
-      '费',
-      '廉',
-      '岑',
-      '薛',
-      '雷',
-      '贺',
-      '倪',
-      '汤',
-      '滕',
-      '殷',
-      '罗',
-      '毕',
-      '郝',
-      '邬',
-      '安',
-      '常',
-      '乐',
-      '于',
-      '时',
-      '傅',
-      '皮',
-      '卞',
-      '齐',
-      '康',
-      '伍',
-      '余',
-      '元',
-      '卜',
-      '顾',
-      '孟',
-      '平',
-      '黄',
-      '和',
-      '穆',
-      '萧',
-      '尹',
-      '姚',
-      '邵',
-      '湛',
-      '汪',
-      '祁',
-      '毛',
-      '禹',
-      '狄',
-      '米',
-      '贝',
-      '明',
-      '臧',
-      '计',
-      '伏',
-      '成',
-      '戴',
-      '谈',
-      '宋',
-      '茅',
-      '庞',
-      '熊',
-      '纪',
-      '舒',
-      '屈',
-      '项',
-      '祝',
-      '董',
-      '梁',
-      '杜',
-      '阮',
-      '蓝',
-      '闵',
-      '席',
-      '季',
-      '麻',
-      '强',
-      '贾',
-      '路',
-      '娄',
-      '危',
+      'Verificación',
+      'Acreditación',
+      'Acreditación de las dimensiones adicionales',
+      'Certificación del sistema de garantía interna de la calidad (SGIC) de centro',
+      'Centro acreditado institucionalmente',
     ];
     var legendData = [];
     var seriesData = [];
@@ -270,10 +131,7 @@ export class ResearchmentStructuresComponent extends PaginatedSearchComponent<
     var name;
 
     for (var i = 0; i < count; i++) {
-      name =
-        Math.random() > 0.65
-          ? makeWord(4, 1) + '·' + makeWord(3, 0)
-          : makeWord(2, 1);
+      name = nameList[i];
       legendData.push(name);
       seriesData.push({
         name: name,
@@ -287,14 +145,5 @@ export class ResearchmentStructuresComponent extends PaginatedSearchComponent<
       seriesData: seriesData,
       selected: selected,
     };
-
-    function makeWord(max, min) {
-      var nameLen = Math.ceil(Math.random() * max + min);
-      var name = [];
-      for (var i = 0; i < nameLen; i++) {
-        name.push(nameList[Math.round(Math.random() * nameList.length - 1)]);
-      }
-      return name.join('');
-    }
   }
 }
