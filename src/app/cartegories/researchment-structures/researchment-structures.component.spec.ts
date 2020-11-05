2fc2d74b861559db44fc27e234929e5eea8ba12a
Atorrecilla <atorrecilla@izertis.com>
2020-11-05 13:34:27 +0100

    [SEMANTMURC-334] Corrección test ResearchmentStructures

--- src/app/cartegories/researchment-structures/researchment-structures.component.spec.ts
+++ src/app/cartegories/researchment-structures/researchment-structures.component.spec.ts
@@ -1,5 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 import { ResearchmentStructuresComponent } from './researchment-structures.component';
 
 describe('ResearchmentStructuresComponent', () => {
@@ -8,9 +13,15 @@ describe('ResearchmentStructuresComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResearchmentStructuresComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot(),
+        ToastrModule.forRoot(),
+        HttpClientModule,
+        FormsModule,
+      ],
+      declarations: [ResearchmentStructuresComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
