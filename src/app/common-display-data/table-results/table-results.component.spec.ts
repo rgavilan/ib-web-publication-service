ca81f8ad999b7e81d6c49eedb71d14ba027d1d54
pteran <pteran@izertis.com>
2020-11-24 15:17:49 +0100

    [SEMANTMURC-416] Testing Upload test

--- src/app/common-display-data/table-results/table-results.component.spec.ts
+++ src/app/common-display-data/table-results/table-results.component.spec.ts
@@ -1,7 +1,8 @@
+import { EventEmitter } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
-import { Page } from 'src/app/_helpers/search';
+import { Direction, FindRequest, Page } from 'src/app/_helpers/search';
 import { TestingHelper } from 'src/app/_helpers/testing.spec';
+import { SparqlResults } from 'src/app/_models/sparql';
 
 import { TableResultsComponent } from './table-results.component';
 
@@ -16,10 +17,86 @@ describe('TableResultsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableResultsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.dataComplete = new SparqlResults();
+    component.dataComplete.head = {
+      vars: []
+    };
+    component.dataComplete.results = {
+      bindings: []
+    };
+    component.dataComplete.results.bindings.length = 12;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  describe('init component without pagination in client', () => {
+
+    beforeEach(() => {
+      component.dataCompleteToShow = [];
+      component.dataCompleteToShow.length = 12;
+      component.pageInfo = new Page();
+      component.pageInfo.content = [];
+      component.pageInfo.size = 10;
+      component.pageInfo.number = 5;
+      fixture.detectChanges();
+
+    });
+
+    it('test', () => {
+      expect(component).toBeTruthy();
+      expect(component.searchResult.length).toEqual(12);
+
+      expect(component.resultObject.uibPage).toEqual(component.pageInfo.number);
+
+    });
+
+  });
+
+  describe('init component with pagination in client', () => {
+
+    beforeEach(() => {
+      component.dataCompleteToShow = [];
+      component.dataCompleteToShow.length = 12;
+      fixture.detectChanges();
+
+    });
+
+    it('test', () => {
+      expect(component).toBeTruthy();
+      expect(component.searchResult.length).toEqual(10);
+      expect(component.resultObject.uibPage).toEqual(1);
+    });
+
+  });
+
+  describe('callShowPageWhenPageChanges', () => {
+
+    beforeEach(() => {
+
+      spyOn(component.pageChanged, 'next').and.callThrough();
+      fixture.detectChanges();
+      component.findRequest = new FindRequest();
+      component.findRequest.pageRequest = {
+        size: 3,
+        page: 5,
+        direction: Direction.ASC,
+        property: 'name'
+      };
+
+      component.callShowPageWhenPageChanges(1);
+    });
+
+    it('without pageInfo', () => {
+      expect(component.findRequest.pageRequest.page).toEqual(1);
+
+    });
+
+    it('with pageInfo', () => {
+      component.pageInfo = new Page();
+      component.pageInfo.content = [];
+      component.pageInfo.size = 10;
+      component.pageInfo.number = 5;
+      expect(component.findRequest.pageRequest.page).toEqual(1);
+      expect(component.pageChanged.next).toHaveBeenCalled();
+    });
   });
 });
