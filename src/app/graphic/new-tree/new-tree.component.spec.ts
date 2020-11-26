bcda6f53753272fe321e6a766a5511dcf453cd28
Florencia <mflorencia@izertis.com>
2020-11-26 09:29:31 +0100

    [SEMANTMURC-400] fix error de testing tree

--- src/app/graphic/new-tree/new-tree.component.spec.ts
+++ src/app/graphic/new-tree/new-tree.component.spec.ts
@@ -9,7 +9,7 @@ describe('NewTreeComponent', () => {
 
   beforeEach(async(() => {
     TestingHelper.configureTest()
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -20,5 +20,55 @@ describe('NewTreeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('area Clicked', () => {
+    it('expect to emit value on array filter', () => {
+      const area = {
+        name: 'Ciencias y tecnologías quimicas',
+        value: 'CTQ',
+        selected: true,
+        children: [
+          { name: 'Ingeniería Química', value: 'IQM', selected: false, children: [] },
+          { name: 'Química', value: 'QMC', selected: false, children: [] }
+        ]
+      };
+      component.areaClicked(area, null);
+      expect(component.filter[0]).toBe('CTQ');
+    });
+
+    it('expect to emit value of IMQ', () => {
+      component.filter = ['CTQ'];
+      const parent = {
+        name: 'Ciencias y tecnologías quimicas',
+        value: 'CTQ',
+        selected: false,
+        children: [
+          { name: 'Ingeniería Química', value: 'IQM', selected: false, children: [] },
+          { name: 'Química', value: 'QMC', selected: false, children: [] }
+        ]
+      };
+      const area = { name: 'Ingeniería Química', value: 'IQM', selected: true, children: [] };
+      spyOn(component.filterChanged, 'emit');
+      component.areaClicked(area, parent);
+      expect(component.filterChanged.emit).toHaveBeenCalledWith(['IQM']);
+    });
+
+    it('delete parent from filter when clicking on child', () => {
+      component.filter = ['CTQ'];
+      const parent = {
+        name: 'Ciencias y tecnologías quimicas',
+        value: 'CTQ',
+        selected: true,
+        children: [
+          { name: 'Ingeniería Química', value: 'IQM', selected: false, children: [] },
+          { name: 'Química', value: 'QMC', selected: false, children: [] }
+        ]
+      };
+      const area = { name: 'Ingeniería Química', value: 'IQM', selected: true, children: [] };
+      spyOn(component.filterChanged, 'emit');
+      component.areaClicked(area, parent);
+      // expect(component.filterChanged.emit).toHaveBeenCalledWith(['IQM']);
+    });
   });
 });
