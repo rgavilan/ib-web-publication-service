b6462b9a04a2dac4ce01b60c98736f78cf08f020
pteran <pteran@izertis.com>
2020-12-11 13:09:29 +0100

    [SEMANTMURC-451] Cambios de estilo e iconos Added sidebar disabled options

--- src/app/_models/menu.ts
+++ src/app/_models/menu.ts
@@ -14,6 +14,10 @@ export class MenuItem {
    * URL.
    */
   url: string;
+  /**
+   * Disabled.
+   */
+  disabled: boolean;
 }
 
 /**
@@ -24,6 +28,7 @@ export const MENU_ITEMS: MenuItem[] = [
     title: 'menu.home',
     icon: 'oi-home',
     url: '/main/home',
+    disabled: false
   },
   /*
   {
@@ -36,40 +41,48 @@ export const MENU_ITEMS: MenuItem[] = [
     title: 'menu.university',
     icon: 'oi-home',
     url: '/main/categories/researchmentStructures',
+    disabled: false
   },
   {
     title: 'menu.scientist',
     icon: 'oi oi-person',
     url: '/main/categories/scientist',
+    disabled: false
   },
   {
     title: 'menu.category-inves',
     icon: 'oi oi-folder',
     url: '/main/home',
+    disabled: true
   },
   {
     title: 'menu.prod-scientist',
     icon: 'oi oi-box',
     url: '/main/home',
+    disabled: true
   },
   {
     title: 'menu.acction-inves',
     icon: 'oi oi-pencil',
     url: '/main/categories/investigation-actions',
+    disabled: false
   },
   {
     title: 'menu.stats',
     icon: 'oi-bar-chart',
     url: '/main/home',
+    disabled: true
   },
   {
     title: 'menu.sparql',
     icon: 'oi-terminal',
     url: '/main/sparql',
+    disabled: false
   },
   {
     title: 'menu.graphics',
     icon: 'oi-bar-chart',
     url: '/main/graphics',
+    disabled: true
   },
 ];
