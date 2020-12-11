b1c00e46fd4d1a803ce3a378ce583ca41631c1f7
Florencia <mflorencia@izertis.com>
2020-12-11 11:20:51 +0100

    [SEMANTMURC-419] - cambios en user service para testing

--- src/app/_services/user.service.spec.ts
+++ src/app/_services/user.service.spec.ts
@@ -2,16 +2,47 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 import { TestingHelper } from '../_helpers/testing.spec';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestingHelper.configureTest();
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+    spyOn(localStorage, 'getItem')
+      .and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem')
+      .and.callFake(mockLocalStorage.removeItem);
+    spyOn(localStorage, 'clear')
+      .and.callFake(mockLocalStorage.clear);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
 });
