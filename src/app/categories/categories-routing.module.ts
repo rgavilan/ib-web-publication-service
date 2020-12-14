7396f344c27aeed22e102f248eb5539322de4720
pteran <pteran@izertis.com>
2020-12-14 13:16:07 +0100

    [SEMANTMURC-456] Revisión de front Link added to page

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -24,6 +24,10 @@ const routes: Routes = [
     component: ProjectsDetailComponent,
   },
   {
+    path: 'researchmentStructures/:proyect/project/:id',
+    component: ProjectsDetailComponent,
+  },
+  {
     path: 'scientist',
     component: ScientistComponent,
   },
