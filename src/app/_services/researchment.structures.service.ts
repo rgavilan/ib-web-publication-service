ac54f71569e26c769cb6dc4109755696a03ac8bb
pteran <pteran@izertis.com>
2020-12-14 13:06:58 +0100

    [SEMANTMURC-455] Mejorar componente tabla Fix bug with pagination in mocks

--- src/app/_services/researchment.structures.service.ts
+++ src/app/_services/researchment.structures.service.ts
@@ -254,7 +254,7 @@ export class ResearchmentStructuresService extends AbstractService {
     const min = ((!!pageRequest.page) ? pageRequest.page - 1 : 0) * pageRequest.size;
     const max = ((!!pageRequest.page) ? pageRequest.page : 1) * pageRequest.size;
     data.results.bindings = data.results.bindings.slice(min, max)
-    page.number = pageRequest.page;
+    page.number = pageRequest.page - 1;
     page.numberOfElements = pageRequest.size;
     page.size = pageRequest.size;
     page.totalElements = dataFiltered.length;
