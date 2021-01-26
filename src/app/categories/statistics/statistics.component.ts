95897c86d5d5e4c308bc628c3a5aa64a4ad32222
Florencia <mflorencia@izertis.com>
2021-01-26 12:53:16 +0100

    [SEMANTMURC-554] - agregado de titulos y graficos base, con datos mock. Agregado de translates

--- src/app/categories/statistics/statistics.component.ts
+++ src/app/categories/statistics/statistics.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
+import { HelperGraphics } from 'src/app/_helpers/helperGraphics';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html'
 })
 export class StatisticsComponent implements OnInit {
-
+  echartOptions2: any;
+  options: any;
   constructor() { }
 
   ngOnInit(): void {
+    const treeData = HelperGraphics.returnSquareData();
+    this.echartOptions2 = HelperGraphics.configChartTree(treeData);
+    // leaflet map
+    this.options = {
+      layers: [
+        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }),
+      ],
+      zoom: 16,
+      center: latLng(43.53573, -5.66152),
+    };
   }
 
 }
